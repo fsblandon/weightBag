51cf1ee3461dd448dd25c1714507c032eebb42db
Fredy Blandon <Fredy.Blandon@ifb-group.com>
2019-05-21 15:56:09 -0500

    add button

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -13,6 +13,8 @@ export class IndexComponent implements OnInit {
   fileExist = true;
   fileN: string;
 
+  fileInput: any;
+
   participante: Participante = {
     cedula: null,
     fecha_exec: null
@@ -40,40 +42,47 @@ export class IndexComponent implements OnInit {
       file: ['']
     });
   }
-
-  addFile(event: any) {
+  addInput(event: any) {
     this.fileExist = false;
     this.fileN = this.dataForm.controls['file'].value;
+    this.fileInput = event.target.files;
+  }
+
+  addFile() {
 
     this.participante.cedula = this.dataForm.controls['cedula'].value;
     console.log(this.participante.cedula);
 
-    const fileInput = <File>event.target.files[0];
+    const fileInput = <File>this.fileInput[0];
     const formData = new FormData();
     formData.append('file', fileInput);
 
-    if (event.target.files && event.target.files[0]) {
+    if (this.fileInput && this.fileInput[0]) {
       if (this.participante.cedula !== null && fileInput !== null) {
         if (fileInput.type === 'text/plain') {
           console.log(fileInput);
           this.service.postData(this.participante, formData ).subscribe(
             (data) => {
               console.log(data);
+              if (data !== null) {
+                const name = 'Lazy_loading_output.txt';
+                const url = URL.createObjectURL(data);
+                const anchor = document.createElement('a');
+                anchor.download = name;
+                anchor.href = url;
+
+                document.body.appendChild(anchor);
+                anchor.click();
+                setTimeout(() => {
+                  document.body.removeChild(anchor);
+                  URL.revokeObjectURL(url);
+                }, 300);
+              } else {
+                console.log('Error in file input');
+              }
               /* const f = new Blob([data], {type: 'text/plain'});
               const url = window.URL.createObjectURL(f);
               window.open(url); */
-              /* const name = 'Lazy_loading_output.txt';
-              const url = URL.createObjectURL(data);
-              const anchor = document.createElement('a');
-              anchor.download = name;
-              anchor.href = url;
-
-              document.body.appendChild(anchor);
-              anchor.click();
-              setTimeout(() => {
-                document.body.removeChild(anchor);
-                URL.revokeObjectURL(url);
-              }, 300); */
             },
             (error) => {
               console.log('error downloading file');
